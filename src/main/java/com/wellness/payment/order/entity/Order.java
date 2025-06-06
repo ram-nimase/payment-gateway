f5c8758b57b563413978193a7cdccb88e4bb27c1
Ram Nimase <nimseram77@gmail.com>
2025-06-06 10:16:58 +0530

    commit code

--- src/main/java/com/wellness/payment/order/entity/Order.java
+++ src/main/java/com/wellness/payment/order/entity/Order.java
@@ -1,116 +1,116 @@
-package com.wellness.payment.order.entity;
-
-
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
-
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name="payment_orders")
-public class Order {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long orderId;
-    private String orderCreatedId;
-    private int amount;
-    private String currency;
-    private String receipt;
-    private String status;
-    private int attempts;
-    private int amount_due;
-    private LocalDateTime createdAt;
-
-    public String getOrderCreatedId() {
-        return orderCreatedId;
-    }
-
-    public void setOrderCreatedId(String orderCreatedId) {
-        this.orderCreatedId = orderCreatedId;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    private LocalDateTime updatedAt;
-
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public String getReceipt() {
-        return receipt;
-    }
-
-    public void setReceipt(String receipt) {
-        this.receipt = receipt;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public int getAttempts() {
-        return attempts;
-    }
-
-    public void setAttempts(int attempts) {
-        this.attempts = attempts;
-    }
-
-    public int getAmount_due() {
-        return amount_due;
-    }
-
-    public void setAmount_due(int amount_due) {
-        this.amount_due = amount_due;
-    }
-
-
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-}
+//package com.wellness.payment.order.entity;
+//
+//
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import java.time.LocalDateTime;
+//
+//
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//@Table(name="payment_orders")
+//public class Order {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long orderId;
+//    private String orderCreatedId;
+//    private int amount;
+//    private String currency;
+//    private String receipt;
+//    private String status;
+//    private int attempts;
+//    private int amount_due;
+//    private LocalDateTime createdAt;
+//
+//    public String getOrderCreatedId() {
+//        return orderCreatedId;
+//    }
+//
+//    public void setOrderCreatedId(String orderCreatedId) {
+//        this.orderCreatedId = orderCreatedId;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        this.createdAt = createdAt;
+//    }
+//
+//    private LocalDateTime updatedAt;
+//
+//
+//    public Long getOrderId() {
+//        return orderId;
+//    }
+//
+//    public void setOrderId(Long orderId) {
+//        this.orderId = orderId;
+//    }
+//
+//    public int getAmount() {
+//        return amount;
+//    }
+//
+//    public void setAmount(int amount) {
+//        this.amount = amount;
+//    }
+//
+//    public String getCurrency() {
+//        return currency;
+//    }
+//
+//    public void setCurrency(String currency) {
+//        this.currency = currency;
+//    }
+//
+//    public String getReceipt() {
+//        return receipt;
+//    }
+//
+//    public void setReceipt(String receipt) {
+//        this.receipt = receipt;
+//    }
+//
+//    public String getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(String status) {
+//        this.status = status;
+//    }
+//
+//    public int getAttempts() {
+//        return attempts;
+//    }
+//
+//    public void setAttempts(int attempts) {
+//        this.attempts = attempts;
+//    }
+//
+//    public int getAmount_due() {
+//        return amount_due;
+//    }
+//
+//    public void setAmount_due(int amount_due) {
+//        this.amount_due = amount_due;
+//    }
+//
+//
+//
+//    public LocalDateTime getUpdatedAt() {
+//        return updatedAt;
+//    }
+//
+//    public void setUpdatedAt(LocalDateTime updatedAt) {
+//        this.updatedAt = updatedAt;
+//    }
+//}
--- src/main/java/com/wellness/payment/razorpay/service/RazorpayService.java
+++ src/main/java/com/wellness/payment/razorpay/service/RazorpayService.java
@@ -2,7 +2,6 @@ package com.wellness.payment.razorpay.service;
 
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
-import com.wellness.payment.order.service.OrderService;
 import jakarta.annotation.PostConstruct;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +27,8 @@ public class RazorpayService {
     private  String keySecret;
    // private final String publicKey;
 
-    @Autowired
-    private OrderService orderService;
+//    @Autowired
+//    private OrderService orderService;
 
 
 //    public RazorpayService(
